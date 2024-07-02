1f01482e22ded46fe5464b22664850c7aa2af052
omii_j <omjuvatkar123@gmail.com>
2024-07-02 18:13:21 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const cookieParser = require("cookie-parser")
+const path = require("path")
 
 const userAuth = require("./Routes/userAuth")
 const messageRoutes = require("./Routes/messageRoutes")
 const userRoutes = require("./Routes/userRoutes")
+
 const {app, server} = require("./socket/socket")
 const connectToMongoose = require("./db/conn")
 
 dotenv.config()
+
 const PORT = process.env.PORT || 5000
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -17,6 +21,14 @@ app.use(cookieParser())
 app.use("/api/v1", userAuth)
 app.use("/api/v1", messageRoutes)
 app.use("/api/v1", userRoutes)
+
+// Serve static files from the frontend directory
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Handle fallback for HTML5 history API (SPA routing)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
 
 server.listen(PORT, ()=>{
   connectToMongoose()
